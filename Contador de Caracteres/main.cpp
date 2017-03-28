ea9bf8ac8574e1a91640d0239c9f13ec4bd1cb1e
GhostRonix <rlr_1995@hotmail.com>
2017-03-28 13:09:30 -0400

    Update main.cpp

--- Contador de Caracteres/main.cpp
+++ Contador de Caracteres/main.cpp
@@ -18,56 +18,14 @@ int main()
    cin>> Cc;
 
    for (int a = 0; Cc[a] != '\0' ; a++)
+   {
 
-    if (Cc[a]== '0')
+    if (Cc[a]== '0' || Cc[a]== '1' || Cc[a]== '2' || Cc[a]== '3' || Cc[a]== '4' || Cc[a]== '5' || Cc[a]== '6' || Cc[a]== '7' || Cc[a]== '8' || Cc[a]== '9' ||)
     licount++;
 
-    for (int a = 0; Cc[a] != '\1' ; a++)
-
-    if (Cc[a]== '1')
-    licount++;
-
-     for (int a = 0; Cc[a] != '\2' ; a++)
-
-    if (Cc[a]== '2')
-    licount++;
+   }
 
 
-
-    /**
-
-
-    for (int a = 0; Cc[a] != '\4' ; a++)
-
-    if (Cc[a]== '4')
-    licount++;
-
-    for (int a = 0; Cc[a] != '\5' ; a++)
-
-    if (Cc[a]== '5')
-    licount++;
-
-    for (int a = 0; Cc[a] != '\6' ; a++)
-
-    if (Cc[a]== '6')
-    licount++;
-
-    for (int a = 0; Cc[a] != '\7' ; a++)
-
-    if (Cc[a]== '7')
-    licount++;
-
-    for (int a = 0; Cc[a] != '\8' ; a++)
-
-    if (Cc[a]== '8')
-    licount++;
-
-    for (int a = 0; Cc[a] != '\9' ; a++)
-
-    if (Cc[a]== '9')
-    licount++;
-
-    */
 
 
 
